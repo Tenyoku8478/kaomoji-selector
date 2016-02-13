59a36d6248dac5970ad78609cb4533290039b0ba
Tenyoku <sh961014@gmail.com>
2016-02-13 15:42:36 +0800

    reimplement using copy & paste

--- js/content.js
+++ js/content.js
@@ -1,14 +1,37 @@
+function processOnTmpArea(func) {
+    var $focused = $(document.activeElement);
+    console.log($focused);
+    var $tmpArea = $('<textarea>')
+        .css('position', 'fixed')
+        .css('left', '50%')
+        .css('top', '50%')
+        .appendTo($('body'));
+    $tmpArea.focus();
+    var res = func($tmpArea);
+    $tmpArea.remove();
+    $focused.focus();
+    return res;
+}
+
+function getClipboard() {
+    return processOnTmpArea(function($tmpArea) {
+        document.execCommand('paste');
+        return $tmpArea.val();
+    });
+}
+function setClipboard(text) {
+    processOnTmpArea(function($tmpArea) {
+        $tmpArea.val(text).select();
+        document.execCommand('copy');
+    });
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.command == "insertKaomoji") {
-            var field = document.activeElement;
-            var $field = $(field);
-            if($field.val) {
-                var begin = field.selectionStart;
-                var end = field.selectionEnd;
-                var text = $field.val();
-                $field.val(text.substring(0, begin)+request.kaomoji+text.substring(end, text.length));
-                field.selectionStart = field.selectionEnd = begin+request.kaomoji.length;
-            }
+            var prevClipboard = getClipboard();
+            setClipboard(request.kaomoji);
+            document.execCommand('paste');
+            setClipboard(prevClipboard);
         }
     });
